ff412de82cb54315393bb51b43019d2812382568
monicapousada <monicapousada300@gmail.com>
2024-12-10 17:19:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tarea4navegacinconfragments/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.tarea4navegacinconfragments;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BottomNavigationView menu = findViewById(R.id.menu);
+        menu.inflateMenu(R.menu.navigation_menu);
+
+        menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.rojo){
+                    getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).replace(R.id.fragmentContainerView2, FragmentoRojo.class, null).commit();
+                } else if (item.getItemId()==R.id.azul) {
+                    getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).replace(R.id.fragmentContainerView2, FragmentAzul.class, null).commit();
+                } else{
+                    getSupportFragmentManager().beginTransaction().setReorderingAllowed(true).replace(R.id.fragmentContainerView2, FragmentVerde.class, null).commit();
+                }
+
+                return false;
+            }
+        });
+    }
+}
